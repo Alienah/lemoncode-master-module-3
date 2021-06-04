393c7071c87778d5ef69609898071a9e16a205b1
Aida Albarran Blanco <AIDAABL@ext.inditex.com>
2021-06-04 22:06:40 +0200

    [feature] add logo and image loader

--- src/header.js
+++ src/header.js
@@ -1,8 +1,20 @@
 import { getUser } from './utils/getUser';
+import logo from './assets/logo.png'
+
+const createLogoContent = () => {
+  const logoContainer = document.getElementById('logo-container');
+  const img = document.createElement('img');
+  img.src = logo;
+  logoContainer.appendChild(img);
+}
+
+const createUserContent = (id) => {
+  const userContainer = document.getElementById('user-container');
+  const user = getUser(id);
+  userContainer.innerHTML = `${user.name.charAt(0).toUpperCase()} ${user.lastName.charAt(0).toUpperCase()}`
+}
 
 export const createHeader = () => {
-  const logoContainer = document.querySelector('.logo');
-  const userContainer = document.querySelector('.user');
-  const user = getUser("id1");
-  userContainer.innerHTML = `${user.name.charAt(0).toUpperCase()} ${user.lastName.charAt(0).toUpperCase()}`
+  createLogoContent();
+  createUserContent("id1");
 }
